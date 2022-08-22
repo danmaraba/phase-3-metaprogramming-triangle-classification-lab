2f0e6b43663a94d96d64cdb5f2aef9b1a4bcb394
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-08-22 06:36:55 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
   # write code here
+  attr_reader :length_x, :length_y,:length_z
+  def initialize (length_x,length_y,length_z)
+    @length_x = length_x
+    @length_y = length_y
+    @length_z = length_z
+  end
+
+  def kind
+    validate
+    if length_x == length_y && length_y ==length_z
+      :equilateral
+    elsif length_x == length_y || length_y  == length_z || length_z == length_x 
+      :isosceles
+    else  
+      :scalene
+    end
+  end
+
+  def validate
+    true_triangle = [(length_x + length_y > length_z), (length_x + length_z > length_y),(length_y + length_z > length_x)]
+    [length_x,length_y,length_z].each do |side|
+      true_triangle << false if side <= 0
+      raise TriangleError if true_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle inequality failed"
+    end
+  end
+
+
 end
